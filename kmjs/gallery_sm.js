1b6c84a80233224818b0d37917a578655dd04c3b
Katya <katyaskvo@gmail.com>
2014-09-03 12:21:08 -0400

    first adoption for mobil devices + directories reorganization

---
+++ kmjs/gallery_sm.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+
+	$('#list_view').justifiedGallery({
+		rowHeight: 80,
+		maxRowHeight: 0,
+		margins: 25,
+		lastRow: 'nojustify',
+		fixedHeight: false,
+		captions: true,
+		randomize: false,
+		sizeRangeSuffixes: ''
+	});
+
+});
